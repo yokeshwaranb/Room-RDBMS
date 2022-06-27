50dbd3249bd63e9e2c2de163eb2afeac35ef4f5a
yokesh7373@gmail.com <gitHub@1>
2022-06-27 20:10:51 +0530

    Updated Director Entity With School Name.

--- app/src/main/java/com/training/roomrdbms/entities/Director.java
+++ app/src/main/java/com/training/roomrdbms/entities/Director.java
@@ -1,15 +1,20 @@
 package com.training.roomrdbms.entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "Director")
 public class Director {
+
+    @NonNull
     @PrimaryKey(autoGenerate = false)
     private String directorName;
+    private String schoolName;
 
-    public Director(String directorName) {
+    public Director(@NonNull String directorName, String schoolName) {
         this.directorName = directorName;
+        this.schoolName = schoolName;
     }
 
     public String getDirectorName() {
@@ -18,5 +23,13 @@ public class Director {
 
     public void setDirectorName(String directorName) {
         this.directorName = directorName;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
     }
 }
