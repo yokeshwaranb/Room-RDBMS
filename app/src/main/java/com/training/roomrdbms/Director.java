5324c66eb48b46d852a4feb992be04d819a95dfa
yokesh7373@gmail.com <gitHub@1>
2022-06-27 19:42:34 +0530

    Created Director Entity.

---
+++ app/src/main/java/com/training/roomrdbms/Director.java
@@ -0,0 +1,22 @@
+package com.training.roomrdbms;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Director")
+public class Director {
+    @PrimaryKey(autoGenerate = false)
+    private String directorName;
+
+    public Director(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+}
