f2e3074acb165296aa9e8f2fd8b0dcec94d03eb0
yokesh7373@gmail.com <gitHub@1>
2022-06-27 22:14:14 +0530

    Added NonNull in School Entity

--- app/src/main/java/com/training/roomrdbms/entities/School.java
+++ app/src/main/java/com/training/roomrdbms/entities/School.java
@@ -1,10 +1,12 @@
 package com.training.roomrdbms.entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class School {
+    @NonNull
     @PrimaryKey(autoGenerate = false)
     private String schoolName;
 
